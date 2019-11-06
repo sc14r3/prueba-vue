b56da47de73b56ef1f7a5ed9e7ae5c2cab7327bd
Luis Angel Romero Escalona <angel.romero.es93@gmail.com>
2019-11-06 10:43:28 -0600

    Inicio de prueba

---
+++ src/App.vue
@@ -0,0 +1,129 @@
+<!--<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="red"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>-->
+<template>
+  <v-app>
+    <!-- Start of Navigation -->
+    <nav>
+      <!-- Start of app toolbar -->
+      <v-toolbar app>
+        <v-toolbar-side-icon
+                @click.stop="drawer = !drawer"
+                class="hidden-md-and-up"
+        ></v-toolbar-side-icon>
+        <v-toolbar-title class="headline text-uppercase"
+        >Vuetify Demo</v-toolbar-title
+        >
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat to="/" exact>Home</v-btn>
+          <v-btn flat to="/about">About</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <!-- End of app toolbar -->
+
+      <!-- Start of mobile side menu -->
+      <v-navigation-drawer app v-model="drawer" right>
+        <!-- Menu title -->
+        <v-toolbar flat>
+          <v-list>
+            <v-list-tile>
+              <v-list-tile-title class="title">Menu</v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+        </v-toolbar>
+        <v-divider></v-divider>
+        <!-- Menu Links -->
+        <v-list>
+          <v-list-tile to="/" exact>
+            <v-list-tile-action>
+              <v-icon>home</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>Home</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile to="/about">
+            <v-list-tile-action>
+              <v-icon>description</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>About</v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <!-- End of mobile side menu -->
+    </nav>
+    <!-- End of Navigation -->
+
+    <v-content>
+      <!-- Display view pages here based on route -->
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: "App",
+    data() {
+      return {
+        drawer: false // Hide mobile side menu by default
+      };
+    }
+  };
+</script>
